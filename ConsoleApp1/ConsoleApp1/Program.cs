8b0ff8c937c6a541ac2d0a201baa7e7e1acf79cd
koeyhzz <153969877+koeyhzz@users.noreply.github.com>
2023-12-26 15:19:03 +0900

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello, World!");     // 한줄을 출력하는 코드
+            Console.WriteLine("박진혁입니다");
+
+            Console.WriteLine("라라라ㅏㄹ");
+
+            Console.Write("나나나"); // 글자를 출력하는 코드
+            Console.Write("다다다");
+
+            Console.WriteLine("라라라\n마마마");
+            
+            // 변수(variable)
+            // 변하는 숫자, 메모리에 기록해둔 값
+
+            // 키보드 입력을 받아서 input이라는 변수에 기록하기
+              string input = Console.ReadLine();
+            Console.WriteLine(input);    */    //input 변수의 내용을 출력하기
+
+
+            // 실습
+            // 시작하면 이름을 물어보고 이름을 3번 출력하는 코드 만들어보기 
+
+            Console.WriteLine("이름을 입력하세요.");
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+            Console.WriteLine(input);
+        }
+    }
+}
+
+// 주석 코드에 포함되지 않는다.
+
+/*
+ 여러줄을 주석으로 처리하는 방법  
+ */
+
+/// 여러줄을 주석으로 처리하는 방법
+/// 
+
+
